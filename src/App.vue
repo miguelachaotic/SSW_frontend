c2ad75d938e9086755adce4a4291494d8eddfe54
= <=>
2025-02-28 12:03:01 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref(null);
+
+</script>
+
+<template>
+  <form>
+    <label for="name-input">Introduce tu nombre: </label>
+    <input id="name-input" v-model="name" @input="console.log(name)" type="text" placeholder="Introduce your name">
+  </form>
+</template>
+
+<style scoped>
+</style>
