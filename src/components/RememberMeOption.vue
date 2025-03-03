7f3008dc94a8bc8cf134601c412cea724299a450
Marcau04 <marcaugamer@gmail.com>
2025-03-03 10:48:29 +0100

    Implementación vista login

---
+++ src/components/RememberMeOption.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="inner_login_container" id="remember_user">
+    <input id="inner_checkbox" type="checkbox">
+        <label for="inner_checkbox">
+            Remember me
+        </label>
+  </div>
+</template>
+
+<style scoped>
+#remember_user {
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  padding-left: 27%;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,44 @@
 <script setup>
 
+import EmailInput from '@/components/EmailInput.vue';
+import NameInput from '@/components/NameInput.vue';
+import PasswordInput from '@/components/PasswordInput.vue';
+import RemembermeOption from '@/components/RemembermeOption.vue';
+import UsernameInput from '@/components/UsernameInput.vue';
+
 </script>
 
 <template>
-
+    <div class="login_container">
+        <h1>REGISTER</h1>
+        <form action="">
+            <NameInput/>
+            <UsernameInput/>
+            <EmailInput/>
+            <PasswordInput/>
+            <RemembermeOption/>
+            <div class="inner_login_container">
+                <button type="submit">Register now!</button>
+            </div>
+        </form> 
+    </div>
 </template>
 
 <style scoped>
+.inner_login_container button[type="submit"] {
+  width: 80%;
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  background: #333;
+  color: white;
+  cursor: pointer;
+  border-radius: 4px;
+  margin-top: 10px;
+}
+
+.inner_login_container button[type="submit"]:hover {
+  background: #545454;
+}
 
 </style>
